b8ae3fdfe74dfaac9253f66ee14f1b0b2c997ee1
PhanAnh1811 <tuananh.61779@gmail.com>
2021-08-25 14:58:22 +0700

    react-native

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './src/components/Task'
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Today's Task */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'android'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder='Write a task'
+          value={task}
+          onChangeText={text => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +61,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#F9F9F9',
+  },
+
+  tasksWrapper: {
+    paddingHorizontal: 20,
+    paddingTop: 50,
+  },
+
+  sectionTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+
+  items: {
+    marginTop: 30,
+  },
+
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
     justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+
+  addText: {
+
   },
 });
---
+++ src/components/Task.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+const Task = (props) => {
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+
+            <View style={styles.circular}>
+
+            </View>
+        </View>
+    )
+}
+
+export default Task
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor:'#FFFFFF',
+        borderRadius:10,
+        padding: 15,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginBottom:20,
+        alignItems:'center',
+    },
+
+    itemLeft: {
+        flexDirection:'row',
+        alignItems:'center',
+        flexWrap:'wrap'
+    },
+
+    square: {
+        width:24,
+        height:24,
+        backgroundColor:'#55BCF666',
+        opacity:0.4,
+        borderRadius:5,
+        marginRight:15,
+    },
+    
+    itemText:{
+        maxWidth:'80%',
+    },
+    circular: {
+        width:12,
+        height:12,
+        borderColor:'#55BCF6',
+        borderWidth:2,
+        borderRadius:5,
+    },
+})
